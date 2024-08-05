622eefccce411d3c5e5ec5ab928460f1b50e56f7
javohir-ergashboyev <ergashboyevjavohirbek@gmail.com>
2024-08-05 10:21:03 +0500

    updated

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     description="A Byte Pair Encoding (BPE) tokenizer package for CPU and GPU.",
     author="Javokhir",
     author_email="machinemind60@gmail.com",
-    url="https://github.com/machineminduzb/bpe_tokenizer",
+    url="https://github.com/machineminduzb_v2/bpe_tokenizer",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 )
